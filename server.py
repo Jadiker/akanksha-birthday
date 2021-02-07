df4cca779df9f8245d22bbdb3397eff1d1571896
Jadiker <happylondon4@gmail.com>
2021-02-06 22:07:56 -0500

    updated for 2021

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['EXPLAIN_TEMPLATE_LOADING'] = True
 
 
 @app.route("/")
@@ -13,5 +14,5 @@ if __name__ == "__main__":
     # port = int(os.environ.get('PORT', 5000))
     # print("about to run")
     # app.run(host='0.0.0.0', port=port)
-    
+
     app.run(debug=False)
